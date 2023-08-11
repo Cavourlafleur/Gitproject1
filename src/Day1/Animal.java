2df78fb56005f7331b78dab8353a0ecfc899cb2b
cavour <cavourlafleur@outlook.com>
2023-08-11 16:49:21 -0400

    testing

--- src/Day1/Animal.java
+++ src/Day1/Animal.java
@@ -1,11 +1,7 @@
 package Day1;
 
 public class Animal {
-    public static void main(String[] args) {
-        for (int i = 0; i <5 ; i++) {
-            System.out.println(i);
 
-            System.out.println(" new commit 1");
 
         }
     }
